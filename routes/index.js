a97971a70fa62ddc80338ef4a6b2a80ea06b048e
kris1803 <78277770+kris1803@users.noreply.github.com>
2022-04-02 19:28:20 +0200

    added bike desc & dynamic checkout button

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const stripe = require('stripe')('sk_test_51KjeyfDKE8PPQdZ15ZpsMHLZRaIyFLglTIigO
 let YOUR_DOMAIN = '';
 let urlRead = false;
 dataBike = [
-  {name: 'BIK045',  url: 'images/bike-1.jpg', price: 679, priceid: 'price_1KjfPyDKE8PPQdZ127udQcVo'},
-  {name: 'ZOOK07',  url: 'images/bike-2.jpg', price: 999, priceid:'price_1KjfQoDKE8PPQdZ1pwDqK4Gs'},
-  {name: 'TITANS',  url: 'images/bike-3.jpg', price: 799, priceid:'price_1KjfRCDKE8PPQdZ1Au4ymdrX'},
-  {name: 'CEWO',  url: 'images/bike-4.jpg', price: 1300, priceid: 'price_1KjfRSDKE8PPQdZ14OK1EIlk'},
-  {name: 'AMIG39',  url: 'images/bike-5.jpg', price: 479, priceid: 'price_1KjfRpDKE8PPQdZ1TtVGx6BX'},
-  {name: 'LIK099',  url: 'images/bike-6.jpg', price: 869, priceid: 'price_1KjfSVDKE8PPQdZ1TSvha7Ms'},
+  {name: 'BIK045',  url: 'images/bike-1.jpg', price: 679, priceid: 'price_1KjfPyDKE8PPQdZ127udQcVo', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
+  {name: 'ZOOK07',  url: 'images/bike-2.jpg', price: 999, priceid:'price_1KjfQoDKE8PPQdZ1pwDqK4Gs', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
+  {name: 'TITANS',  url: 'images/bike-3.jpg', price: 799, priceid:'price_1KjfRCDKE8PPQdZ1Au4ymdrX', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
+  {name: 'CEWO',  url: 'images/bike-4.jpg', price: 1300, priceid: 'price_1KjfRSDKE8PPQdZ14OK1EIlk', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
+  {name: 'AMIG39',  url: 'images/bike-5.jpg', price: 479, priceid: 'price_1KjfRpDKE8PPQdZ1TtVGx6BX', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
+  {name: 'LIK099',  url: 'images/bike-6.jpg', price: 869, priceid: 'price_1KjfSVDKE8PPQdZ1TSvha7Ms', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda veritatis officiis atque consequuntur ducimus quasi neque, libero enim illum!' },
 ]
 
 /* GET home page. */
